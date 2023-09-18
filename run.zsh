09cc0449a0afe53e019796aa83927a2ce5e96107
Kevin Le <kevin.k.le.2020@gmail.com>
2023-09-18 02:01:24 -0500

    udpated zsh script

--- run.zsh
+++ run.zsh
@@ -1,1 +1,2 @@
-g++ -std=c++11 ./main.cpp -o ./main && ./main < ./inputs/size8.txt
+g++ -std=c++11 ./main.cpp -o ./main && ./main < ./inputs/randValues.txt
+# g++ -std=c++11 ./main.cpp -o ./main && ./main < ./inputs/size8.txt
